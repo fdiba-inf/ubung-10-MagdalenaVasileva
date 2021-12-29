2316d45b166e8ad6cef09800171362e74ceb851e
MagdalenaVasile <92403839+MagdalenaVasileva@users.noreply.github.com>
2021-12-29 23:16:26 +0000

    ccccc

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -1,31 +1,34 @@
 package exercise10;
 
 public class Ellipse extends Figure {
-
     public Ellipse() {
       super(new Point(0, 0), 1, 1);
     }
 
     public Ellipse(Point startPoint, double a, double b) {
-      super(new Point(startPoint), a, b);
+      super(startPoint, a, b);
     }
 
     public Ellipse(Ellipse otherEllipse) {
       super(new Point(otherEllipse.startPoint), otherEllipse.side1, otherEllipse.side2);
     }
 
+    @Override
     public double calculatePerimeter() {
         return Math.PI * (3.0 * (side1 + side2) - Math.sqrt((3.0 * side1 + side2) * (side1 + 3.0 * side2)));
     }
 
+    @Override
     public double calculateArea() {
         return Math.PI * side1 * side2;
     }
 
+    @Override
     public String getType() {
         return (side1 == side2) ? "Circle" : "Ellipse";
     }
 
+    @Override
     public boolean equal(Figure otherFigure) {
       if (otherFigure instanceof Ellipse) {
         return super.equal(otherFigure);
@@ -43,6 +46,6 @@ public class Ellipse extends Figure {
         double part1 = (clickX - centerX) * (clickX - centerX) / side1 * side1;
         double part2 = Math.pow(clickY - centerY, 2) / Math.pow(side2, 2);
 
-        return part1 + part2 <= 1;
+        return (part1 + part2) <= 1;
     }
 }
